89636878a31770d5420cd17466327fbf352a832e
AtLeastEddie8 <ee3de33@icloud.com>
2023-10-20 23:07:26 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,24 @@ fn main() {
     println!("The value of x is: {x}"); 
     x = 6;
     println!("The value of x is: {x}");
+
+    let y = 5;
+
+    let y = y + 1;
+
+    {
+        let y = y * 2;
+        println!("The value of y in the inner scope is: {y}");
+    }
+    
+    println!("The value of y is: {y}");
+
+    let spaces = "   ";
+    println!("The string of spaces is: {spaces}");
+    let spaces = spaces.len();
+    println!("The number of spaces is: {spaces}");
+    /* // wont compile bc of mut keyword, has to be let and then shadowed to change data type
+    let mut spaces2 = "   ";
+    spaces2 = spaces2.len();
+    */
 }
